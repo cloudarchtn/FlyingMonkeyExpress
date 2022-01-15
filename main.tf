4d6694de08e76864eeb37d3386aa254b5f173cfd
David Purkey <37219194+cloudarchtn@users.noreply.github.com>
2022-01-15 17:51:34 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,19 +8,13 @@ provider "google" {
   credentials = var.FME_ADMIN_CRED
 }
 
-module "vpc" {
-    source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "~> 4.0.1"
-
+resource "google_compute_network" "vpc_network" {  
     project_id   = var.ADMIN_PROJECT
     network_name = var.FME_NETWORK_NAME
     auto_create_subnetworks = false
     shared_vpc_host = false
 }
 
-resource "google_compute_network" "vpc_network" {  
-name = var.FME_NETWORK_NAME
-}
 resource "google_compute_subnetwork" "public-subnetwork" {
 name = var.FME_SN_1
 ip_cidr_range = "10.0.0.0/24"

c7f39af60e5ebb7b23d36d2c4d5c243f667fa2e2
David Purkey <37219194+cloudarchtn@users.noreply.github.com>
2022-01-15 16:26:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 
+provider "google" {
+  project = var.ADMIN_PROJECT
+  region  = var.REGION
+  zone    = var.ZONE
+  credentials = var.FME_ADMIN_CREDS
+}
+resource "google_compute_network" "vpc_network" {
+name = "FME-network"
+}
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "fme-subnet1"
+ip_cidr_range = var.FME_SN_1
+region = var.REGION
+network = google_compute_network.vpc_network.name
+}

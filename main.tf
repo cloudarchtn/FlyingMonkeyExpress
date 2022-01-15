c255500ae49cb34d2879392fb3dcf0fbc8531e49
David Purkey <37219194+cloudarchtn@users.noreply.github.com>
2022-01-15 17:27:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,23 +4,22 @@ module "vpc" {
     version = "~> 2.0.0"
 
     project_id   = var.ADMIN_PROJECT
-    network_name = "example-vpc"
-
+    network_name = var.FME_NETWORK_NAME
+    auto_create_subnetworks = false
     shared_vpc_host = false
 }
 
 provider "google" {
   project = var.ADMIN_PROJECT
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  region  = var.REGION
+  zone    = var.ZONE
   credentials = var.FME_ADMIN_CRED
 }
-resource "google_compute_network" "vpc_network" {
-region = var.REGION  
-name = "fme-network"
+resource "google_compute_network" "vpc_network" {  
+name = var.FME_NETWORK_NAME
 }
 resource "google_compute_subnetwork" "public-subnetwork" {
-name = "fme-subnet1"
+name = var.FME_SN_1
 ip_cidr_range = "10.0.0.0/24"
 region = var.REGION
 network = google_compute_network.vpc_network.name

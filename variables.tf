85ab10b93427c5e649a9bc062f48ffe9fcc26aa2
David Purkey <37219194+cloudarchtn@users.noreply.github.com>
2022-01-15 16:43:54 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,44 @@
-
+variable "FME_SN_1" {
+  type = string
+  default = ""
+}
+variable "FME_SN_2" {
+  type = string
+  default = ""
+}
+variable "FME_SN_3" {
+  type = string
+  default = ""
+}
+variable "FME_SN_4" {
+  type = string
+  default = ""
+}
+variable "MACHINE_TYPE" {
+  type = string
+  default = ""
+}
+variable " INSTANCE_NAME" {
+  type = string
+  default = ""
+}
+variable " ADMIN_PROJECT" {
+  type = string
+  default = ""
+}
+variable " APP_PROJECT" {
+  type = string
+  default = ""
+}
+variable " REGION" {
+  type = string
+  default = ""
+}
+variable " ZONE" {
+  type = string
+  default = ""
+}
+variable " MACHINE_IMAGE" {
+  type = string
+  default = ""
+}

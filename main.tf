12652b287a017cae2db55f2b7640612d31636e7f
David Purkey <37219194+cloudarchtn@users.noreply.github.com>
2022-01-16 16:04:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "google_compute_instance" "default" {
 
 ###### deploy based on machine image
 resource "google_compute_instance_from_machine_image" "tpl" {
-  provider = google-beta
+//  provider = google-beta
   name     = "instance-from-machine-image"
   zone     = "us-central1-a"
 
